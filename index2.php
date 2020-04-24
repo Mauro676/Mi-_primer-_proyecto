196536f2ece5044428d623217ec776a549f423c0
Mauro676 <63746127+Mauro676@users.noreply.github.com>
2020-04-23 23:12:45 -0300

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,184 @@
+<!DOCTYPE HTML>
+<!--
+	Visualize by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>mi primera pagina web</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+	<head>
+  <title>Menu Desplegable</title>
+  <style type="text/css">
+   
+   * {
+    margin:0px;
+    padding:0px;
+   }
+   
+   #header {
+    margin:auto;
+    width:500px;
+    font-family:Arial, Helvetica, sans-serif;
+   }
+   
+   ul, ol {
+    list-style:none;
+   }
+   
+   .nav > li {
+    float:left;
+   }
+   
+   .nav li a {
+    background-color:#000;
+    color:#fff;
+    text-decoration:none;
+    padding:10px 12px;
+    display:block;
+   }
+   
+   .nav li a:hover {
+    background-color:#434343;
+   }
+   
+   .nav li ul {
+    display:none;
+    position:absolute;
+    min-width:140px;
+   }
+   
+   .nav li:hover > ul {
+    display:block;
+   }
+   
+   .nav li ul li {
+    position:relative;
+   }
+   
+   .nav li ul li ul {
+    right:-140px;
+    top:0px;
+   }
+   
+  </style>
+ </head>
+ <body>
+  <div id="header">
+   <ul class="nav">
+    <li><a target="_blank" href="https://www.anywhere.com/es/ecuador/travel-guide/gente-cultura">Inicio</a></li>
+    <li><a href="">Acerca de</a>
+     <ul>
+      <li><a target="_blank" href="https://www.bbc.com/mundo/noticias-49978717">Crisis de Ecuador</a></li>
+      <li><a target="_blank" href="https://youtu.be/OhuCJVDWu7I">VIDEOS</a></li>
+       <ul>
+       </ul>
+      </li>
+     </ul>
+    </li>
+    
+   </ul>
+  </div>
+ </body>
+</html>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+			<nav class="navegacion">
+				<!-- Header -->
+					<header id="header">
+						<span class="logo"><img src="logo.png" alt="" /></span>
+						<h1>En esta <strong></strong>pàgina web <a href="">Daremos a presentar informaciòn sobre costumbres,usos,mitos y leyendas</a><br />
+						de la Cultura de Ecuador!! </h1>
+						<ul lass="iconsc">
+							<li><a target="_blank" href="https://www.presidencia.gob.ec/"> <img src="images/ecuador.jpg" width="50" height="50">
+							<li><a target="_blank" href="https://www.facebook.com/profile.php?id=100015280183300"><span class="label">Facebook</span></a></li>
+							
+
+						</ul>
+					</header>
+					</nav>
+				<!-- Main -->
+					<section id="main">
+
+						<!-- Thumbnails -->
+							<section class="thumbnails">
+								<div>
+									
+									<a target="_blank" href="https://leyendadeterror.net/leyendas-ecuatorianas/">	
+										<img src="images/mitos.jpg" alt="">
+										<h3>En esta parte se brindarà informaciòn sobre 
+											mitos y leyendas</h3> 
+									</a>
+									
+									
+									
+									<a target="_blank" href="https://www.viajejet.com/costumbres-y-tradiciones-ecuador/">
+										<img src="images/tradiciones.jpg" alt="" />
+										<h3>Al hacer click nos direcciona a dicha informacion al respecto</h3>
+									</a>
+									
+								</div>
+								<div>
+									<a href="">
+										<img src="images/vestimenta.jpg" alt="" />
+										
+									</a>
+									
+									<a target="_blank" href="http://vestimentafdelecuador.blogspot.com/">
+									<h3>Como se visten los Ecuatorianos</h3>
+									
+									
+
+									<a target="_blank" href="https://www.rebeccaadventuretravel.com/gastronomia-del-ecuador/?lang=es">
+										<img src="images/gastronomia.jpg" alt="" />
+										<h3>Gastronomia</h3>
+									</a>
+									
+									<a target="_blank" href="https://www.goraymi.com/es-ec/ecuador/fiestas-populares-en-el-ecuador-a5a374430">
+										<img src="images/fiestas.jpg" alt="" />
+										<h3>Fiestas Populares</h3>
+									</a>
+								</div>
+								<div>
+									<a target="_blank" href="http://leyendasdeecuador.blogspot.com/">
+										<img src="images/leyendas.jpg" alt="" />
+										<h3>Leyendas</h3>
+									</a>
+									
+									<a target="_blank" href="http://musicandote.com/musica-ecuatoriana/">
+										<img src="images/musica.jpg" alt="" />
+										<h3>Mùsica</h3>
+									</a>
+								</div>
+							</section>
+
+					</section>
+		<?php
+
+include("formulario.php");
+					?>
+				<!-- Footer -->
+					<footer id="footer">
+						<br><p>&copy; correo: maurooojedaa@gmail.com --- gabyyromeroo25@gmail.com</a></p></footer></br>
+						
+						<p> CULTURAS   / CONTACTO  / INFORMACIÒN </a></p>
+						
+						<center><p>&copy; © 1989 - 2019 Ideal Education Group S.L. Todos los derechos reservados Política de privacidad </a></p></center>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
